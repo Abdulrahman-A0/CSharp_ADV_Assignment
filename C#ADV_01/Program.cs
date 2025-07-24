4a7ad3675b79710e5e496293bc31f1c0d4c84a3b
Abdulrahman <abdelrhmanelhshamy@gmail.com>
2025-07-24 14:31:25 +0300

    Add project files.

---
+++ C#ADV_01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C_ADV_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Range<int> range = new Range<int>(1, 5);
+            Console.WriteLine(range.IsInRange(1));
+            Console.WriteLine(range.IsInRange(0));
+            Console.WriteLine(range.IsInRange(5));
+            Console.WriteLine(range.Length());
+
+            Range<double> rng = new Range<double>(1.2, 5.7);
+            Console.WriteLine(rng.Length());
+
+        }
+    }
+}
---
+++ C#ADV_01/Range.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace C_ADV_01
+{
+    public class Range<T> where T : IComparable<T>, INumber<T>
+    {
+        public T MinValue { get; set; }
+        public T MaxValue { get; set; }
+
+        public Range(T minValue, T maxValue)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(MinValue) >= 0 && value.CompareTo(MaxValue) <= 0;
+        }
+
+        public T Length()
+        {
+            return MaxValue - MinValue;
+        }
+    }
+}
